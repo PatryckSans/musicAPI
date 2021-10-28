52432c5705c11cf480a4591a3c91d1ae092f9643
Patryck <patrycksans@gmail.com>
2021-10-28 12:38:41 -0300

    migration

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes, Model } = require("sequelize");
+
+
+class User extends Model {
+    static init(connection) {
+super.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+}, {
+    sequelize : connection
+})
+    }
+}
+module.exports = User;
